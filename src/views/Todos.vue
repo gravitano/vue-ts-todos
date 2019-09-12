48579543acceb7d55558af5599b6a76aeda70efd
Warsono <warsono16694@gmail.com>
2019-09-12 09:21:37 +0700

    change "markAsCompleted" with toggleCompleted and change UI

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -2,20 +2,30 @@
   <div>
     <ul>
       <li v-for="todo in todos" :key="todo.title">
-        {{ todo.title }}
-        <button
-          v-if="!todo.completed"
-          title="Mark as completed"
-          @click="markAsCompleted(todo)"
-        >
-          &#10004;
-        </button>
-        <span
-          title="Mark as uncompleted"
-          @click="markAsUncompleted(todo)"
-          v-else
-          >&#10004;</span
-        >
+        <s v-if="todo.completed">
+          {{ todo.title }}
+        </s>
+        <span v-else>
+          {{ todo.title }}
+        </span>
+        <input
+          type="checkbox"
+          v-model="todo.completed"
+          @click="toggleCompleted(todo)"
+        />
+        <!--        <button-->
+        <!--          v-if="!todo.completed"-->
+        <!--          title="Mark as completed"-->
+        <!--          @click="markAsCompleted(todo)"-->
+        <!--        >-->
+        <!--          &#10004;-->
+        <!--        </button>-->
+        <!--        <span-->
+        <!--          title="Mark as uncompleted"-->
+        <!--          @click="markAsUncompleted(todo)"-->
+        <!--          v-else-->
+        <!--          >&#10004;</span-->
+        <!--        >-->
       </li>
     </ul>
     <input type="text" v-model="newTodo" @keydown.enter="addTodo" />
@@ -57,6 +67,10 @@ export default class Todos extends Vue {
 
   markAsUncompleted(todo) {
     todo.completed = false;
+  }
+
+  toggleCompleted(todo) {
+    todo.completed = !todo.completed;
   }
 }
 </script>
