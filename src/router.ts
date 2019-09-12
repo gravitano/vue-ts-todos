fee5d5396867ccfe75bbd4a21186b3001b7fcf7f
Warsono <warsono16694@gmail.com>
2019-09-12 09:03:59 +0700

    added todo list

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Todos from './views/Todos.vue';
 
 Vue.use(Router);
 
@@ -11,7 +12,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: Todos,
     },
     {
       path: '/about',
---
+++ src/views/Todos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.title">
+        {{ todo.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component
+export default class Todos extends Vue {
+  todos = [
+    {
+      title: 'Go to store',
+    },
+    {
+      title: 'Go to library',
+    },
+    {
+      title: 'Go to school',
+    },
+  ];
+}
+</script>
+
+<style scoped></style>
